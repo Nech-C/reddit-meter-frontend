c15d01af6472d896f7c4449e8080d87868faebd5
Nech-C <xiaokunchen@outlook.com>
2025-08-04 09:39:56 -0400

    chore: add test stack

---
+++ src/test/setup.js
@@ -0,0 +1,10 @@
+// src/test/setup.js
+import '@testing-library/jest-dom'
+import { server } from '../mocks/server'
+
+// Establish API mocking before all tests.
+// beforeAll(() => server.listen())
+// // Reset any request handlers after each test
+// afterEach(() => server.resetHandlers())
+// // Clean up after the tests are finished.
+// afterAll(() => server.close())
